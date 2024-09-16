c014f41ca1479ecf14c370207178ac9c569039b0
NaokiTM <ntsome21@gmail.com>
2024-09-16 21:20:15 +0100

    sharinan eye attempt

--- src/app/components/Eye.tsx
+++ src/app/components/Eye.tsx
@@ -5,9 +5,17 @@ function Eye() {
   return (
     <div className='eye-container'>
         <div className='sclera'>
-            <div className='iris iris-animation'>
+            <div className='iris'>
+                <div className='tail1'></div>
+                {/* <div className='tail2'></div>
+                <div className='tail3'></div> */}
                 <div className='iris-shadow'></div>
-                <div className='pupil'></div>
+                <div className='pupil'>
+                    <div className='inner-tail1'></div>
+                    <div className='inner-tail2'></div>
+                    <div className='inner-tail3'></div>
+                    <div className='inner-pupil'></div>
+                </div>
             </div>
         </div>
     </div>

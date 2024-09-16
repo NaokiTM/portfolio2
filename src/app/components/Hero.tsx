9b28a4e6ebcd8eafb64082d24dd74b61206a04bf
NaokiTM <ntsome21@gmail.com>
2024-09-17 00:16:45 +0100

    created gradient component file, made the export keyword part of function declaration

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-function Hero() {
+export default function Hero() {
   return (
     <>
       <div className='flex justify-center items-center text-160px font-medium tracking-tighter'>Naoki Miyagawa</div>
     </>
   )
 }
-
-export default Hero
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero";
+import Gradient from "./components/Gradient";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Gradient />
     </>
   );
 }
